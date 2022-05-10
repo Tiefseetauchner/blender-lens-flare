89c204ad64d354838fcd5bb7b08ad25421718068
tauchner <7229@htl.rennweg.at>
2022-05-10 09:55:40 +0200

    Boilerplate stuff and things

--- __init__.py
+++ __init__.py
@@ -14,37 +14,96 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-bl_info = {
-        "name": "My Awesome Add-on",
-        "description": "Single line describing my awesome add-on.",
-        "author": "Aaron Powell",
-        "version": (1, 0),
-        "blender": (2, 80, 0),
-        "location": "Properties > Render > My Awesome Panel",
-        "warning": "", # used for warning icon and text in add-ons panel
-        "wiki_url": "http://my.wiki.url",
-        "tracker_url": "http://my.bugtracker.url",
-        "support": "COMMUNITY",
-        "category": "Render"
-        }
-
 import bpy
+from . import addon_updater_ops
 
-#
-# Add additional functions here
-#
+bl_info = {
+    "name": "Lens Flare Boi",
+    "description": "Adds a Lens Flare Compositor Node",
+    "author": "LukeChriswalker",
+    "version": (0, 1),
+    "blender": (3, 0, 0),
+    "location": "Compositor",
+    "warning": "",  # used for warning icon and text in add-ons panel
+    "tracker_url": "https://github.com/Tiefseetauchner/blender-lens-flare/issues",
+    "support": "COMMUNITY",
+    "category": "Node"
+}
+
+
+@addon_updater_ops.make_annotations
+class DemoPreferences(bpy.types.AddonPreferences):
+    """Demo bare-bones preferences"""
+    bl_idname = __package__
+
+    # Addon updater preferences.
+
+    auto_check_update = bpy.props.BoolProperty(
+        name="Auto-check for Update",
+        description="If enabled, auto-check for updates using an interval",
+        default=False)
+
+    updater_interval_months = bpy.props.IntProperty(
+        name='Months',
+        description="Number of months between checking for updates",
+        default=0,
+        min=0)
+
+    updater_interval_days = bpy.props.IntProperty(
+        name='Days',
+        description="Number of days between checking for updates",
+        default=7,
+        min=0,
+        max=31)
+
+    updater_interval_hours = bpy.props.IntProperty(
+        name='Hours',
+        description="Number of hours between checking for updates",
+        default=0,
+        min=0,
+        max=23)
+
+    updater_interval_minutes = bpy.props.IntProperty(
+        name='Minutes',
+        description="Number of minutes between checking for updates",
+        default=0,
+        min=0,
+        max=59)
+
+    def draw(self, context):
+        layout = self.layout
+
+        # Works best if a column, or even just self.layout.
+        mainrow = layout.row()
+        col = mainrow.column()
+
+        # Updater draw function, could also pass in col as third arg.
+        addon_updater_ops.update_settings_ui(self, context)
+
+
+classes = (
+    DemoPreferences,
+)
+
 
 def register():
-    from . import properties
-    from . import ui
-    properties.register()
-    ui.register()
+    # Addon updater code and configurations.
+    # In case of a broken version, try to register the updater first so that
+    # users can revert back to a working version.
+    addon_updater_ops.register(bl_info)
+
+    # Register the example panel, to show updater buttons.
+    for cls in classes:
+        addon_updater_ops.make_annotations(cls)  # Avoid blender 2.8 warnings.
+        bpy.utils.register_class(cls)
+
 
 def unregister():
-    from . import properties
-    from . import ui
-    properties.unregister()
-    ui.unregister()
+    # Addon updater unregister.
+    addon_updater_ops.unregister()
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
 
 if __name__ == '__main__':
     register()
